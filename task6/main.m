9459672895769f51ebe5ccb0f40f45f60da54394
ryeebak <ryeebak@gmail.com>
2024-05-28 22:30:31 +0300

    Update main.m

--- task6/main.m
+++ task6/main.m
@@ -3,12 +3,12 @@ clear, clc
 k = 1;
 T = 0.5;
 T1 = 0.5;
-W0 = tf(k, [T*T1, T+T1,1,0]);
+W0 = tf(k, [T*T1, T+T1,1,0])
 sisotool(W0)
 % C = 0.5
 
 kp=1;
 Ta=1;
 Td=10;
-Wp=tf([kp*Ta+Td, kp],[Ta, 1]);
+Wp=tf([kp*Ta+Td, kp],[Ta, 1])
 sisotool(W0,Wp);
